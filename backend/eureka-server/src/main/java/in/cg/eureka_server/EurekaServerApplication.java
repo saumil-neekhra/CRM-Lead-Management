bbf4891328dbbe53122c9d10bf964f82e78647d9
Saumil Neekhra <saumilneekhra2004@gmail.com>
2026-06-19 08:44:18 +0530

    Eureka Server and Config Server Initialized

---
+++ backend/eureka-server/src/main/java/in/cg/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,14 @@
+package in.cg.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
